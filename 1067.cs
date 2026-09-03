8803741c237916cfbf3c375446d8e9a1396dfc2c
Mickael <r4c4vm5ctv@privaterelay.appleid.com>
2026-09-03 17:25:58 -0300

    1067

---
+++ 1067.cs
@@ -0,0 +1,10 @@
+using System;
+
+class URI {
+    static void Main(string[] args) {
+        int x = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= x; i += 2) {
+            Console.WriteLine(i);
+        }
+    }
+}
