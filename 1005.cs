3c4041f34096859a4c0d33ebc52270c336488894
Mickael <r4c4vm5ctv@privaterelay.appleid.com>
2026-09-03 17:26:31 -0300

    1005

---
+++ 1005.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+
+        double MEDIA = (A * 3.5 + B * 7.5) / 11.0;
+
+        Console.WriteLine("MEDIA = " + MEDIA.ToString("F5"));
+    }
+}
