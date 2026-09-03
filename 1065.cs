4dd9e3d718d6b6a8133eac2f8d3ea88db343fb5f
Mickael <r4c4vm5ctv@privaterelay.appleid.com>
2026-09-03 17:25:11 -0300

    1065

---
+++ 1065.cs
@@ -0,0 +1,14 @@
+using System;
+
+class URI {
+    static void Main(string[] args) {
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            int valor = int.Parse(Console.ReadLine());
+            if (valor % 2 == 0) {
+                count++;
+            }
+        }
+        Console.WriteLine($"{count} valores pares");
+    }
+}
