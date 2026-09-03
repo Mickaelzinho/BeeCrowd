5b1d8b99a7ba5b9aa550a797b7afd5315d886ee2
Mickael <r4c4vm5ctv@privaterelay.appleid.com>
2026-09-03 17:28:01 -0300

    1002

---
+++ 1002.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double raio = double.Parse(Console.ReadLine());
+
+        double area = 3.14159 * raio * raio;
+
+        Console.WriteLine("A=" + area.ToString("F4"));
+    }
+}
